93ae7fc78f72f8224bcd8d10eddfd5c984035fb2
Zlatin Balevsky <zlatinb@gmail.com>
2021-03-20 16:41:15 +0000

    read the location from jpackage.app-path and launch an i2p router

--- java/net/i2p/router/MacLauncher.java
+++ java/net/i2p/router/MacLauncher.java
@@ -1,10 +1,19 @@
 package net.i2p.router;
 
 import java.io.*;
+import java.util.*;
 
 public class MacLauncher {
+    /** this is totally undocumented */
+    private static final String APP_PATH = "jpackage.app-path";
+
     public static void main(String[] args) throws Exception {
-        var here = new File(".");
-        System.out.println("I'm in " + here.getAbsolutePath());
+        String path = System.getProperty(APP_PATH,"unknown");
+        File f = new File(path);
+        File contents = f.getParentFile().getParentFile();
+        File resources = new File(contents, "Resources");
+
+        System.setProperty("i2p.dir.base", resources.getAbsolutePath());
+        RouterLaunch.main(args);
     }
 }
