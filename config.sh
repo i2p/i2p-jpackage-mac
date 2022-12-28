fa567cb536cf28076ac20191027f2ff70368635c
idk <hankhill19580@gmail.com>
2022-12-28 01:35:01 +0000

    don't over-write builder provided environment variables in config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,17 @@
 #! /usr/bin/env sh
 
-I2P_SIGNER=signer@mail.i2p
-I2P_VERSION=2.0.0
-I2P_BUILD_NUMBER=1
+if [ -z $I2P_SIGNER ]; then
+    I2P_SIGNER=signer@mail.i2p
+fi
+if [ -z $I2P_VERSION ]; then
+    I2P_VERSION=2.0.0
+fi
+if [ -z $I2P_BUILD_NUMBER ]; then
+    I2P_BUILD_NUMBER=1
+fi
+# Uncomment/Edit this line to include the signer in the config file
+# I2P_SIGNER=signer@mail.i2p
+# Uncomment/Edit this line to include the version number in the config file
+# I2P_VERSION=2.0.0
+# Uncomment/Edit this line to include the build number in the config file
+# I2P_BUILD_NUMBER=1
