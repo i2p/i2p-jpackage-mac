53c388888196985d86cfb0c4e83eba4de13a82b6
idk <hankhill19580@gmail.com>
2023-02-01 23:40:13 +0000

    ignore git switch errors, just means we're on master

--- build.sh
+++ build.sh
@@ -69,7 +69,7 @@ I2P_PKG=$HERE/../i2p.i2p-jpackage-mac/pkg-temp
 
 
 cd "$I2P_SRC"
-git switch - 2>/dev/null; true
+git switch - || :
 git pull --tags
 git checkout "i2p-$I2P_VERSION"
 OLDEXTRA=$(find ../i2p.i2p-jpackage-mac -name RouterVersion.java -exec grep 'String EXTRA' {} \;)
