905b50c68e4eb13717764cad60c3f0a43782247b
eyedeekay <hankhill19580@gmail.com>
2025-06-30 16:23:46 -0400

    debug buildscript

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e 
 git describe --tags `git rev-list --tags --max-count=1` || exit 1
-export GITHUB_TAG=$(git describe --tags `git rev-list --tags --max-count=1` | sed 's|i2p||g' | tr -d a-z- | cut -d - -f 1,2)
+export GITHUB_TAG=$(git describe --tags `git rev-list --tags --max-count=1` | cut -d - -f 1,2 | sed 's|i2p||g' | tr -d a-z-)
 echo "tag is: $GITHUB_TAG"
 if echo "$GITHUB_TAG" | grep -q '.\..\..'; then
     PUBLISH_VERSION="$GITHUB_TAG"
